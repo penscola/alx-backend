435bfb6574ab0d83b99c088f87116248afbe2783
penscola <penscolashackletonfelix@gmail.com>
2024-03-05 21:34:55 +0300

    minor changes

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -2,23 +2,22 @@
 import { createClient } from 'redis';
 
 const client = createClient();
+const EXIT_MSG = 'KILL_SERVER';
 
 client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
 });
 
-const publishMessage = (message, time) => {
-  setTimeout(() => {
-    console.log(`About to send ${message}`);
-    client.publish('holberton school channel', message);
-  }, time);
-};
-
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-publishMessage('Holberton Student #1 starts course', 100);
-publishMessage('Holberton Student #2 starts course', 200);
-publishMessage('KILL_SERVER', 300);
-publishMessage('Holberton Student #3 starts course', 400);
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === EXIT_MSG) {
+    client.unsubscribe();
+    client.quit();
+  }
+});
